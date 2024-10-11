836aaec03c11ea028eafef758599fec6cd688dd4
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-10-11 16:12:41 +0530

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Mukthar Amiyan on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBooksTests/MyBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksTests.swift
+//  MyBooksTests
+//
+//  Created by Mukthar Amiyan on 11/10/24.
+//
+
+import Testing
+@testable import MyBooks
+
+struct MyBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
